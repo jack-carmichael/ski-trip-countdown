4d50a81d1e517d4a135666e0a28d954416260861
Jack Carmichael <jack.carmie@gmail.com>
2026-07-06 15:27:50 -0700

    idk man

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()]
+	plugins: [enhancedImages(), sveltekit({
+		adapter: adapter(),
+	})]
 });
